86dfed59cd0d3983a363b06959686a2359c0a64b
PramodDhyani <pramod.dhyani@gmail.com>
2025-01-30 02:48:49 +0530

    Used on_button_click

--- frontend/main.py
+++ frontend/main.py
@@ -1,24 +1,30 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
 class MyApp(App):
+    def on_button_click(self, instance):
+        # This will print the text of the button that was clicked
+        print(f"{instance.text} button clicked!")
+
     def build(self):
-        # Create a layout that arranges widgets vertically
         layout = BoxLayout(orientation='vertical')
 
-        # Create a label widget
-        label = Label(text="Welcome to Kivy!")
+        # Create multiple buttons
+        button1 = Button(text="Button 1")
+        button2 = Button(text="Button 2")
+        button3 = Button(text="Button 3")
 
-        # Create a button widget
-        button = Button(text="Click Me!")
+        # Bind the same method to all buttons
+        button1.bind(on_press=self.on_button_click)
+        button2.bind(on_press=self.on_button_click)
+        button3.bind(on_press=self.on_button_click)
 
-        # Add the label and button to the layout
-        layout.add_widget(label)
-        layout.add_widget(button)
+        # Add buttons to layout
+        layout.add_widget(button1)
+        layout.add_widget(button2)
+        layout.add_widget(button3)
 
-        # Return the layout to be displayed
         return layout
 
 # Run the app
